0dcbe7c2453dfab79e459b68624f29a788868f02
Maryam2028 <mrpaki2028@gmail.com>
2026-01-01 06:27:12 -0800

    Loops logic learned

---
+++ 2026-01-01/loops.js
@@ -0,0 +1,33 @@
+for (let i = 0; i <= 100; i++) {
+    // console.log(`(${i}) = I love Coding!`);
+}
+
+let i = 0
+
+while (i <= 100) {
+   // console.log(`(${i}) I love Coding!. I learn and Successfull in 2026.`);
+   //  console.log(`(${i})2026 are my Year!`);
+    
+    i++
+}
+
+for(let i = 0; i <= 10; i++){
+    // console.log("Stranger loves Learning");
+    
+}
+
+let j = 0;
+
+do {
+   // console.log(`(${j})Work Hard untill You achieve Success!`);
+    j ++
+} while (j <= 10);
+
+for(let k = 0; k <= 10; k++){
+    console.log("Yes");
+    
+    if(k === 3){
+        console.log(`${k} equals to 3`);
+        break;
+    }
+}
